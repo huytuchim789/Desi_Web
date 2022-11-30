a5677e7040c5135121dd8502d5157549f61ab114
Huy Tu <tranhuytu242000@gmail.com>
2022-11-30 23:50:12 +0700

    dashboard

--- src/components/CustomDrawler/index.jsx
+++ src/components/CustomDrawler/index.jsx
@@ -1,62 +1,54 @@
-import * as React from "react";
-import PropTypes from "prop-types";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
-
+import * as React from 'react'
+import PropTypes from 'prop-types'
+import Tabs from '@mui/material/Tabs'
+import Tab from '@mui/material/Tab'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter'
+import PersonIcon from '@mui/icons-material/Person'
+import KeyIcon from '@mui/icons-material/Key'
+import styles from './../CustomDrawler/drawler.module.css'
 function a11yProps(index) {
   return {
     id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
+    'aria-controls': `simple-tabpanel-${index}`,
+  }
 }
-
+function LinkTab(props) {
+  return (
+    <Tab
+      component="a"
+      onClick={(event) => {
+        event.preventDefault()
+      }}
+      {...props}
+    />
+  )
+}
+const tabs = [
+  { icon: <PersonIcon />, label: 'Hồ sơ cá nhân' },
+  { icon: <BusinessCenterIcon />, label: 'Quản lý việc làm' },
+  { icon: <KeyIcon />, label: 'Đổi mật khẩu' },
+]
 export default function BasicTabs() {
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = React.useState(0)
 
   const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+    setValue(newValue)
+  }
 
   return (
-    <Box sx={{ width: "100%" }}>
+    <Box sx={{ width: '100%' }} className={styles.drawler}>
       <Tabs
         value={value}
         onChange={handleChange}
         aria-label="basic tabs example"
         orientation="vertical"
       >
-        <Tab label="Item One" {...a11yProps(0)} />
-        <Tab label="Item Two" {...a11yProps(1)} />
-        <Tab label="Item Three" {...a11yProps(2)} />
+        {tabs.map((t) => (
+          <LinkTab icon={t.icon} iconPosition="start" label={t.label} />
+        ))}
       </Tabs>
     </Box>
-  );
+  )
 }
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,20 +1,131 @@
-import { Grid } from "@mui/material";
-import CustomDrawer from "components/CustomDrawler";
-import Footer from "components/Footer";
-import Header from "components/Header";
-import React from "react";
-
+import {
+  Box,
+  Grid,
+  InputAdornment,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material'
+import CustomDrawer from 'components/CustomDrawler'
+import Footer from 'components/Footer'
+import Header from 'components/Header'
+import React from 'react'
+import styles from './../Dashboard/dashboard.module.css'
+import Tabs from '@mui/material/Tabs'
+import Tab from '@mui/material/Tab'
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined'
+import CustomTable from 'components/CustomTable'
+const tabs = ['Việc làm đã ứng tuyển', 'Việc làm yêu thích ']
 export default function Dashboard() {
+  const [value, setValue] = React.useState(0)
+  const [choice, setChoice] = React.useState(0)
+  const [choices, setChoices] = React.useState([
+    {
+      num: 1,
+      label: 'Chờ phản hồi',
+      color: '#FFC107',
+    },
+    {
+      num: 1,
+      label: 'Đã nhận',
+      color: '#00AB55',
+    },
+    {
+      num: 1,
+      label: 'Bị từ chối ',
+      color: '#FF4842',
+    },
+    {
+      num: 1,
+      label: 'Tất cả',
+      color: '#DFE3E8',
+    },
+  ])
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue)
+  }
+  const handleChoice = (event, newValue) => {
+    setChoice(newValue)
+  }
   return (
     <>
       <Header />
       <Grid container>
-        <Grid item lg={2}>
+        <Grid item lg={2.5}>
           <CustomDrawer />
         </Grid>
-        <Grid item lg={10}></Grid>
+        <Grid item lg={9.5}>
+          <Stack className={styles.dashboard} spacing="30px">
+            <Typography variant="h5">Quản lý việc làm </Typography>
+            <Typography className={styles.normal_text} fontWeight={400}>
+              Bạn có thể quản lý các công việc yêu thích hoặc công việc đã ứng
+              tuyển tại đây
+            </Typography>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              aria-label="basic tabs example"
+            >
+              {tabs.map((t) => (
+                <Tab label={t}></Tab>
+              ))}
+            </Tabs>
+            <Stack spacing="10px">
+              <Tabs
+                aria-label="basic tabs example"
+                value={choice}
+                onChange={handleChoice}
+                className={styles.tab_header}
+              >
+                {choices.map((t) => (
+                  <Tab
+                    label={
+                      <Stack direction="row" spacing="15px">
+                        <Stack
+                          alignItems="center"
+                          justifyContent="center"
+                          sx={{
+                            width: '21px',
+                            height: '21px',
+                            borderRadius: '6px',
+                            backgroundColor: t.color,
+                          }}
+                        >
+                          {t.num}
+                        </Stack>
+                        <Typography
+                          className={styles.normal_text}
+                          fontWeight={600}
+                        >
+                          {t.label}
+                        </Typography>
+                      </Stack>
+                    }
+                  ></Tab>
+                ))}
+              </Tabs>
+              <Box>
+                <TextField
+                  hiddenLabel
+                  id="outlined-start-adornment"
+                  fullWidth
+                  placeholder="Tìm kiếm việc làm cụ thể..."
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <SearchOutlinedIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Box>
+              <CustomTable />
+            </Stack>
+          </Stack>
+        </Grid>
       </Grid>
       <Footer />
     </>
-  );
+  )
 }
